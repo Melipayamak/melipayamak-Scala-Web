b0e9ad2a53861860084092b178e9df4f001ee6fc
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2018-06-02 18:56:22 +0430

    Update RestClient.scala

--- RestClient.scala
+++ RestClient.scala
@@ -9,46 +9,46 @@ import com.fasterxml.jackson.databind.JsonNode
 
 
 class RestClient(//implements WSBodyReadables, WSBodyWritables
-                 val ws: Nothing) {
+                 val ws: WSClient) {
   final private val baseRestUrl = "https://rest.payamak-panel.com/api/SendSMS/"
-  private var username = null
-  private var password = null
+  private var username = ""
+  private var password = ""
 
-  def initCred(username: Nothing, password: Nothing): Unit = {
+  def initCred(username: String, password: String): Unit = {
     this.username = username
     this.password = password
   }
 
-  def SendSMS(to: Nothing, from: Nothing, text: Nothing, flash: Boolean): Nothing = {
-    val json = Json.newObject.put("username", username).put("password", password).put("to", to).put("from", from).put("text", text).put("isFlash", Boolean.toString(flash))
+  def SendSMS(to: String, from: String, text: String, flash: Boolean): CompletionStage[WSResponse] = {
+    val json = Json.newObject.put("username", username).put("password", password).put("to", to).put("from", from).put("text", text).put("isFlash", flash.toString())
     ws.url(baseRestUrl + "SendSMS").addHeader("Content-Type", "application/json").post(json)
     //.thenApply(response ->
     //ok("result: " + response.getBody())//.asJson().findValues("full_name")
     //);    
   }
 
-  def GetDeliveries2(recId: Long): Nothing = {
+  def GetDeliveries2(recId: Long): CompletionStage[WSResponse] = {
     val json = Json.newObject.put("username", username).put("password", password).put("recId", String.valueOf(recId))
     ws.url(baseRestUrl + "GetDeliveries2").addHeader("Content-Type", "application/json").post(json)
   }
 
-  def GetMessages(location: Int, from: Nothing, index: Nothing, count: Boolean): Nothing = {
-    val json = Json.newObject.put("username", username).put("password", password).put("location", String.valueOf(location)).put("from", from).put("index", index).put("count", Boolean.toString(count))
+  def GetMessages(location: Int, from: String, index: String, count: Boolean): CompletionStage[WSResponse] = {
+    val json = Json.newObject.put("username", username).put("password", password).put("location", String.valueOf(location)).put("from", from).put("index", index).put("count", count.toString())
     ws.url(baseRestUrl + "GetMessages").addHeader("Content-Type", "application/json").post(json)
   }
 
-  def GetCredit: Nothing = {
+  def GetCredit: CompletionStage[WSResponse] = {
     val json = Json.newObject.put("username", username).put("password", password)
 
     ws.url(baseRestUrl + "GetCredit").addHeader("Content-Type", "application/json").post(json)
   }
 
-  def GetBasePrice(recId: Long): Nothing = {
+  def GetBasePrice(recId: Long): CompletionStage[WSResponse] = {
     val json = Json.newObject.put("username", username).put("password", password)
     ws.url(baseRestUrl + "GetBasePrice").addHeader("Content-Type", "application/json").post(json)
   }
 
-  def GetUserNumbers(recId: Long): Nothing = {
+  def GetUserNumbers(recId: Long): CompletionStage[WSResponse] = {
     val json = Json.newObject.put("username", username).put("password", password)
     ws.url(baseRestUrl + "GetUserNumbers").addHeader("Content-Type", "application/json").post(json)
   }
